88c97b0a9f11e540b2707cdfb4e3e4e30465d83c
Fatih <16fay61@gmail.com>
2023-07-18 10:39:59 +0200

    aa

--- class-notes/02_OOP/OOP_Inclass.py
+++ class-notes/02_OOP/OOP_Inclass.py
@@ -2,8 +2,66 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 print("-------------------------------------")
+# print("hello world!")
 
-print("hello world!")
+#! Defining class
+
+# class Person:
+#     company="clarusway"
+#     department="IT"
+
+# person1=Person()
+# person2=Person()
+
+# print(person1.company)
+
+# Person.job="developer"
+# print(person1.job)
+
+# person2.location="Germany"
+#print(person1.location)
+
+#! SELF keyword 
+
+# class Person:
+#     company="clarusway"
+#     department="IT"
+
+#     def test(self):
+#         print("test")
+
+#     def set_details(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def get_details(self):
+#         print(self.name,self.age)
+
+#     #! static methods
+#     @staticmethod 
+#     def salute():
+#         print("Hi there!")
+
+
+# person1=Person()
+# person2=Person()
+# # person1.test()
+
+# person1.set_details("barry",40) 
+# person2.set_details("henry",35)
+
+# person2.get_details()
+# person1.salute()
+
+
+#! Special methods (init, str)
+
+class Person:
+   company="clarusway"
+   department="IT"
+
+
+
 
 
 
