916b641b3be1aa8491048de3b8ef7230c88502f4
Fatih <16fay61@gmail.com>
2023-07-27 10:02:30 +0200

    test

---
+++ class-notes/08_CBV/student_api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import home, student_api, student_create, student_delete, student_detail, student_update
+
+urlpatterns = [
+    #! Function views endpoints
+#     path("", home ),
+#     path("student-list", student_api, name="liste"),
+#     path("student-create", student_create, name="create"),
+#     path("student-detail/<int:pk>", student_detail, name="detail"),
+#     path("student-delete/<int:pk>", student_delete, name="delete"),
+#     path("student-update/<int:pk>", student_update, name="update")
+    #! Class views endpoints
+    
+
+
+ ]
