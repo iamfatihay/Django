20995bef25e5a9f02692415d58ada3b3b780d06f
Fatih <16fay61@gmail.com>
2023-07-28 10:33:21 +0200

    aa

--- class-notes/09_Task_Tracker/todoApp/models.py
+++ class-notes/09_Task_Tracker/todoApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+
+    PRIORITY=(
+        (1, "High"),
+        (2, "Medium"),
+        (3, "Low")
+    )
+    task=models.CharField(max_length=100)
+    description=models.TextField(null=True)
+    priority=models.IntegerField(choices=PRIORITY, default=2)
+    created_date=models.DateTimeField(auto_now_add=True) #! Ilk task eklenirken eklenir sadece
+    updated_date=models.DateTimeField(auto_now=True)  #! her islem degisikliginde tarih ekler
+    is_done=models.BooleanField() 
+
+    def __str__(self):
+        return f"{self.task}"
