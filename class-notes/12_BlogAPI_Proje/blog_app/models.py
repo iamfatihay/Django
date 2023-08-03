109ddf0efee821600a5cce64a6b751e9fae65165
Fatih <16fay61@gmail.com>
2023-08-03 10:08:02 +0200

    a

--- class-notes/12_BlogAPI_Proje/blog_app/models.py
+++ class-notes/12_BlogAPI_Proje/blog_app/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Blog category'
+        verbose_name_plural = 'Blog categories'
+
     def __str__(self):
         return self.name
 
@@ -17,6 +21,10 @@ class Blog(models.Model):
     content=models.TextField(null=True)
     created_date=models.DateTimeField(auto_now_add=True, null=True) #! Ilk task eklenirken eklenir sadece
     updated_date=models.DateTimeField(auto_now=True)  #! her islem degisikliginde tarih ekler
+
+    class Meta:
+        verbose_name = 'Blog '
+        verbose_name_plural = 'Blogs '
 
     def __str__(self):   #! Admin panelde nasil gorecegimizi belirliyoruz.
         return f"{self.title}"
