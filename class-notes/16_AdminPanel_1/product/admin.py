8a49152d1cf8db80a08f4a534817e353cd48b74a
Fatih <16fay61@gmail.com>
2023-08-09 11:42:36 +0200

    aa

--- class-notes/16_AdminPanel_1/product/admin.py
+++ class-notes/16_AdminPanel_1/product/admin.py
@@ -24,8 +24,56 @@ class ProductAdmin(ModelAdmin):
     search_help_text = "Arama yapmak icin burayi kullaniniz"
     ordering = ["id"]  #! "id" >> ASC   "-id" >> DESC
     list_per_page = 20  #! Sayfa basi product sayisi
-    list_max_show_all = 200 #! Tumunu goster derken max gosterim sayisi
-    date_hierarchy = "create_date"
+    list_max_show_all = 200  #! Tumunu goster derken max gosterim sayisi
+    date_hierarchy = "create_date"  #! Tarih field olmak zorunda
+    prepopulated_fields = {
+        "slug": ["name"]
+    }  #! name i doldurunca otomatik slug olusturuyor
+    # Form liste görüntüleme:
+    fields = (
+        ("name", "is_in_stock"),
+        ("slug"),
+        ("description"),
+    )
+    """
+    #* Detayli form liste görüntüleme:
+    fieldsets = (
+        ('General Settings', {
+            # "classes": ['wide'],
+            "fields": (
+                ('name', 'is_in_stock'),
+                ('slug'),
+            )
+        }),
+        ('Optional Settings', {
+            "classes": ['collapse'],
+            "fields": (
+                ('description'),
+            ),
+            'description': "You can use this section for optionals settings"
+        }),
+    )
+    """
+
+    def set_stock_in(self, request, queryset):
+        count = queryset.update(is_in_stock=True)
+        self.message_user(request, f'{count} adet "Stokta Var" olarak işaretlendi.')
+
+    def set_stock_out(self, request, queryset):
+        count = queryset.update(is_in_stock=False)
+        self.message_user(request, f'{count} adet "Stokta Yok" olarak işaretlendi.')
+
+    actions = ("set_stock_in", "set_stock_out")
+    set_stock_in.short_description = "İşaretli ürünleri stoğa ekle"
+    set_stock_out.short_description = "İşaretli ürünleri stoktan çikar"
+
+    def added_days_ago(self, object):
+        from django.utils import timezone
+
+        different = timezone.now() - object.create_date
+        return different.days
+
+    list_display += ("added_days_ago",)
 
 
 admin.site.register(Product, ProductAdmin)
