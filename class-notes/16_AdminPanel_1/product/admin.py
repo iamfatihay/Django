8f5c48597e8d5cc3692751bf624c806d2260dff3
Fatih <16fay61@gmail.com>
2023-08-09 10:33:14 +0200

    aa

--- class-notes/16_AdminPanel_1/product/admin.py
+++ class-notes/16_AdminPanel_1/product/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from .models import *
 
-admin.site.register(Product)
+
+# * #########################
+# *  PRODUCT
+# * #########################
+
+
+class ProductAdmin(ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "description",
+        "is_in_stock",
+        "create_date",
+        "update_date",
+    )  #! Tablo sutunlari
+    list_editable = ["is_in_stock"]  #! Tablo uzerinde edit islemi yapabilme
+    list_display_links = ["id", "name"]  #! Kayda gitmek icin linkleme
+    list_filter = ["is_in_stock", "create_date", "update_date"]
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+# * #########################
+# *  PRODUCT
+# * #########################
 admin.site.register(Review)
