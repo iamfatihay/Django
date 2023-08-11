9d1ddc3ad254f2c18f8792ea416fbd1b558dd6cf
Fatih <16fay61@gmail.com>
2023-08-11 10:36:52 +0200

    a

--- class-notes/18_Tests/flights/tests.py
+++ class-notes/18_Tests/flights/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from .views import FlightView
 
-# Create your tests here.
+#! class isminin sonu "TestCase" olmali
+
+class FlightTestCase(APITestCase):
+    def setUp(self):
+        self.factory=APIRequestFactory
+
+    #! fonksiyon yazarken de "test" ile baslamali isim
+    def test_flight_list(self):
+        request=self.factory.get("/flights")
+        response=
