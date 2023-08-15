9341a91821a04f1e4653aa79f0f62caf8a3918f5
Fatih <16fay61@gmail.com>
2023-08-15 14:24:01 +0200

    aa

--- projects/tutorial-app-fs/api/tutorials/urls.py
+++ projects/tutorial-app-fs/api/tutorials/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import TutorialMVS 
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+router = DefaultRouter()
+router.register("tutorials", TutorialMVS)
 
-]
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+
+# ]
--- projects/tutorial-app-fs/api/tutorials/views.py
+++ projects/tutorial-app-fs/api/tutorials/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .serializers import TutorialSerializer
+from .models import Tutorial
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
+class TutorialMVS(ModelViewSet):
+    queryset= Tutorial.objects.all()
+    serializer_class= TutorialSerializer
