9d8fe8a871281d6c13d8c64edde637e8a3364f00
Fatih <16fay61@gmail.com>
2023-08-17 09:53:41 +0200

    aa

--- class-notes/21_Personnel_App/users/models.py
+++ class-notes/21_Personnel_App/users/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 # Create your models here.
-    
+# from django.contrib.auth.models import AbstractUser
+# from django.conf import settings
+# from datetime import date
+
+
+# class MyUser(AbstractUser):
+#     username = models.CharField(max_length=50, blank=True, null=True, unique=True)
+#     email = models.EmailField(("email address"), unique=True)
+#     native_name = models.CharField(max_length=5)
+#     phone_no = models.CharField(max_length=10)
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+#     def __str__(self):
+#         return "{}".format(self.email)
--- class-notes/21_Personnel_App/users/urls.py
+++ class-notes/21_Personnel_App/users/urls.py
@@ -2,8 +2,9 @@ from django.urls import path,include
 from .views import RegisterView
 
 urlpatterns = [
+    # login/logout
     path('auth/', include('dj_rest_auth.urls')),
+    # Register
     path('register/',RegisterView.as_view()),
-
 
 ]
