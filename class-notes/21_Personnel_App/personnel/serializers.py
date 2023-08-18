a7141c3699a3b4ff383818b86b7b18ed6a31fc6d
Fatih <16fay61@gmail.com>
2023-08-18 11:20:40 +0200

    add

--- class-notes/21_Personnel_App/personnel/serializers.py
+++ class-notes/21_Personnel_App/personnel/serializers.py
@@ -1,17 +1,28 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from models. import Department, Personnel
+from .models import Department, Personnel
+
+#* her serializers da kullanilan ortak degerleri kendimiz ortak kullanilacak bir serializer olusturabiliriz.
+class FixModel(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    user_id = serializers.IntegerField(required = False)
 
 
-class DepartmentSerializer(serializers.ModelSerializer):
+class DepartmentSerializer(FixModel):
     # user = serializers.StringRelatedField()
     # user_id = serializers.IntegerField(required = False)
-
+    
+    #! method field lar sadece read_only dir.
+    personnel_count = serializers.SerializerMethodField()
     class Meta:
         model = Department
         fields = "__all__" 
+    
+    def get_personnel_count(self,obj):
+        return obj.personnels.count()
 
-class PersonnelSerializer(serializers.ModelSerializer):
+
+class PersonnelSerializer(FixModel):
     # user = serializers.StringRelatedField()
     # user_id = serializers.IntegerField(required = False)
 
--- class-notes/21_Personnel_App/personnel/views.py
+++ class-notes/21_Personnel_App/personnel/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .serializers import DepartmentSerializer, PersonnelSerializer
 from rest_framework.generics import ListCreateAPIView
 from .models import Department, Personnel
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class DepartmentView(ListCreateAPIView):
     serializer_class = DepartmentSerializer
     queryset = Department.objects.all()
+    permission_classes = [IsAdminUser,]
 
 
 class PersonnelView(ListCreateAPIView):
