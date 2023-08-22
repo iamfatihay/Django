b314ef9e37b5504a05a7345ee2cebe595cb2c8dc
Fatih <16fay61@gmail.com>
2023-08-22 14:44:52 +0200

    add

--- class-notes/21_Personnel_App/personnel/views.py
+++ class-notes/21_Personnel_App/personnel/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from .serializers import DepartmentSerializer, PersonnelSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Department, Personnel
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from .permissions import IsStaffOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class DepartmentView(ListCreateAPIView):
@@ -20,4 +22,22 @@ class PersonnelView(ListCreateAPIView):
 class Personnel_GPD_UPDATE_View(RetrieveUpdateDestroyAPIView):   #! GPD = Get, post, destroy
     serializer_class = PersonnelSerializer
     queryset = Personnel.objects.all()
-    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticatedOrReadOnly]  #! eger authentice kullanici ise get put delete yapabilir, degilse sadece get
+
+    def put(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if self.request.user.is_staff and (instance.create_user == self.request.user):
+            return self.update(request, *args, **kwargs)
+        else:
+            data = {
+                "message": "You are not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    def delete(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return self.destroy(request, *args, **kwargs)
+        else:
+            data = {
+                "message": "You are not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
