f52aa6386bcbbb3c581f6e205bf7d9eab93a3484
Fatih <16fay61@gmail.com>
2023-08-22 16:47:19 +0200

    up

---
+++ class-notes/23_RenACar_App/rent_car/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+# from .views import PersonnelView,DepartmentView,Personnel_GPD_View,DepartmentPersonnelView
+
+urlpatterns = [
+    # path('department/',DepartmentView.as_view()),
+]
