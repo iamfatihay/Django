efeba49cb391402f78830c633e866d7a529ceb63
Fatih <16fay61@gmail.com>
2023-08-24 12:33:09 +0200

    filter

--- class-notes/24_rentacar_inclass/car_App/views.py
+++ class-notes/24_rentacar_inclass/car_App/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CarSerializer, ReservationSerializer
 from .models import Car, Reservation
+from rest_framework.permissions import IsAdminUser
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
 class CarView(ModelViewSet):
     serializer_class = CarSerializer
     queryset = Car.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ReservationView(ModelViewSet):
