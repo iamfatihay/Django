6174aa04de0100140b548b47a0fbc0ec4fd00677
Fatih <16fay61@gmail.com>
2023-08-24 12:10:57 +0200

    urls

--- class-notes/24_rentacar_inclass/car_App/urls.py
+++ class-notes/24_rentacar_inclass/car_App/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path,include
-# from .views import RegisterView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CarView, ReservationView
+
+router = DefaultRouter()
+router.register("car", CarView)
 
 urlpatterns = [
-    # path('auth/', include('dj_rest_auth.urls')),
-    # path('register/',RegisterView.as_view()),
-
+    # path('', include(router.urls)),
 ]
+urlpatterns += router.urls
--- class-notes/24_rentacar_inclass/main/urls.py
+++ class-notes/24_rentacar_inclass/main/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('car/', include('car_App.urls')),
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
+    path("api/", include("car_App.urls")),
 ]
