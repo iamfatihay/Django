fc313998eee329d2de18280e60446c825b979848
Fatih <16fay61@gmail.com>
2023-08-25 10:37:13 +0200

    add

--- class-notes/24_rentacar_inclass/car_App/views.py
+++ class-notes/24_rentacar_inclass/car_App/views.py
@@ -11,6 +11,21 @@ class CarView(ModelViewSet):
     queryset = Car.objects.all()
     permission_classes = [IsAdminOrReadOnly]
 
+    def get_queryset(self):
+        # queryset = super().get_queryset()   #! 23.satirda "self." yazmassak bu satiri eklemeliyiz!
+        
+        
+        start = self.request.query_params.get('start')
+        end = self.request.query_params.get('end')
+
+        if start and end:
+            not_available=Reservation.objects.filter(end_date__gt=start,start_date__lt=end).values_list('id',flat=True)
+            #! flat true id leri [1,3,7] listeye çeviriyor
+            queryset = self.queryset.exclude(id__in=not_available).filter(availability=True)
+
+        return queryset
+    
+
 
 class ReservationView(ModelViewSet):
     serializer_class = ReservationSerializer
