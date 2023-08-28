7128f9ca31cc55801290871115e76d101c30bce7
Fatih <16fay61@gmail.com>
2023-08-28 10:47:39 +0200

    templates

--- class-notes/25_Templates/templateApp/views.py
+++ class-notes/25_Templates/templateApp/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>welcome to django template</h1>')
+    return HttpResponse("<h1>welcome to django template</h1>")
 
 
 def body(request):
-     context={
-          'name':'yunus'
-          }
-     return render(request,'templateApp/index.html',context)
-    # return render(request,'templateApp/index.html',{'name':'yunus'})
+    context = {
+        "title": "clarusway",
+        "path": "FS",
+        "list": [
+            "yunus",
+            "ozlem",
+            "fatih",
+            "esra",
+            "huseyin",
+            "nihal",
+            "emirhan",
+            "halit",
+            "irfan",
+        ],
+        "dict": {"k1": "value1", "k2": "value2"},
+        "number": 0,
+        "desc": "this template from APP dir"
+    }
+    return render(request, "templateApp/index.html", context)
+
+
+# return render(request,'templateApp/index.html',{'name':'yunus'})
