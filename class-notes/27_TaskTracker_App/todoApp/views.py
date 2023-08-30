1f830f040224b082b93b05bcbbc9fd3785448791
Fatih <16fay61@gmail.com>
2023-08-30 12:25:06 +0200

    todo-done

--- class-notes/27_TaskTracker_App/todoApp/views.py
+++ class-notes/27_TaskTracker_App/todoApp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Todo
 
-
+#!############
 #!### FBV ####
+#!############
 def todo_list(request):
     todos = Todo.objects.all()
     context = {
@@ -42,6 +43,40 @@ def todo_update(request, pk):
             return redirect("todo_list")
     context = {
         "form": form,
-        "todo":todo
+        # "todo":todo
     }
     return render(request, "todoApp/update.html", context)
+
+
+def todo_delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect("todo_list")
+
+#!############
+#!### CBV ####
+#!############
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+class TodoListView(ListView):
+    model = Todo
+    # eger farkli bir template kullanacaksaniz:
+    # template_name = "todoApp/list.html"
+    # template de default object_list olarak kullanilacak
+
+from django.urls import reverse_lazy
+class TodoCreateView(CreateView):
+    model = Todo
+    form_class = TodoForm
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    form_class = TodoForm
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
