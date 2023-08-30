912ef9681e4d5f6726deaf163d4ce43499b22d55
Fatih <16fay61@gmail.com>
2023-08-30 10:00:56 +0200

    todo2

--- class-notes/27_TaskTracker_App/todoApp/urls.py
+++ class-notes/27_TaskTracker_App/todoApp/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
-
+from .views import todo_list
 urlpatterns = [
-
+    path("",todo_list),
 ]
--- class-notes/27_TaskTracker_App/todoApp/views.py
+++ class-notes/27_TaskTracker_App/todoApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+
+#!### FBV ####
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        "todos":todos,
+    }
+    return render(request, "todoApp/list.html", context)
+#   return render(request, html, {{ "todos":todos }} )
+#   return render(request, html, context )
