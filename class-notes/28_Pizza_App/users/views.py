8dd344c7f17f69a433b258024ec35390c264bcb8
Fatih <16fay61@gmail.com>
2023-08-31 12:16:28 +0200

    login-logout added

--- class-notes/28_Pizza_App/users/views.py
+++ class-notes/28_Pizza_App/users/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import UserForm
+from django.contrib.auth import login, logout
 
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    # form = UserCreationForm()
+    form = UserForm()
+
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
-        
+
     context = {"form": form}
     return render(request, "users/register.html", context)
+
+
+def user_login(request):
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect("home")
+    context = {"form": form}
+    return render(request, "users/login.html", context)
+
+
+def user_logout(request):   
+    logout(request)
+    return redirect("home")
