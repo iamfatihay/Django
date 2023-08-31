16e3868fb16d66e5f7214d0489b206567708d2dc
Fatih <16fay61@gmail.com>
2023-08-31 10:53:41 +0200

    register tested

--- class-notes/28_Pizza_App/users/urls.py
+++ class-notes/28_Pizza_App/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import register
 
 urlpatterns = [
-    path('/register', register, name="register"),
+    path('register', register, name="register"),
 ]
