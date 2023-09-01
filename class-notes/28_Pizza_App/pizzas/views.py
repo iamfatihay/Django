a2e56dc4ab36acf070b6381978d51133481348ff
Fatih <16fay61@gmail.com>
2023-09-01 11:25:27 +0200

    toppings of pizza

--- class-notes/28_Pizza_App/pizzas/views.py
+++ class-notes/28_Pizza_App/pizzas/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def home(request):
     return render(request, "pizzas/home.html")
+
+def pizzas(request):
+    pizzas = Pizza.objects.all()
+    context = {
+        'pizzas':pizzas
+    }
+
+    return render(request, "pizzas/pizzas.html", context)
