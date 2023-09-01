6e18a8d791f09e52b50ddab25e4dc0fec3eeb7cc
Fatih <16fay61@gmail.com>
2023-09-01 12:43:44 +0200

    delete and update buttons

--- class-notes/28_Pizza_App/pizzas/views.py
+++ class-notes/28_Pizza_App/pizzas/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Pizza
+from django.shortcuts import render, redirect
+from .models import Pizza, Order
 
 # Create your views here.
 def home(request):
@@ -13,9 +13,39 @@ def pizzas(request):
 
     return render(request, "pizzas/pizzas.html", context)
 
+from .forms import OrderForm
 def order(request, pk):
     pizza = Pizza.objects.get(id=pk)
+    form = OrderForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            order=form.save(commit=False)
+            order.pizza = pizza
+            order.user = request.user
+            order.save()
+            return redirect('home')
+
     context = {
-        'pizza':pizza
+        'pizza':pizza,
+        'form':form
     }
     return render(request, "pizzas/order.html", context)
+
+
+def my_orders(request):
+    orders = Order.objects.filter(user = request.user)
+    context = {
+        'orders':orders,
+    }
+    return render(request, "pizzas/my_orders.html", context)
+
+
+def update(request):
+    
+    return render(request, "pizzas/my_orders.html")
+
+
+def delete(request, pk):
+    order = Order.objects.get(id=pk)
+    order.delete()
+    return redirect('myorders')
