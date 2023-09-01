89b43497a5b33505b003edbb09500adf5e3c3108
Fatih <16fay61@gmail.com>
2023-09-01 09:46:01 +0200

    messages added

--- class-notes/28_Pizza_App/users/views.py
+++ class-notes/28_Pizza_App/users/views.py
@@ -2,33 +2,45 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserForm
 from django.contrib.auth import login, logout
+from django.contrib import messages
 
 
 # Create your views here.
 def register(request):
-    # form = UserCreationForm()
-    form = UserForm()
 
-    if request.method == "POST":
-        form = UserForm(request.POST)
+    # form=UserCreationForm()
+    form=UserForm()
+
+    if request.method=='POST':
+        form=UserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(request,'register successfully')
             return redirect("home")
+        
+    context={
+        'form':form
+    }
+    return render(request,'users/register.html',context)
 
-    context = {"form": form}
-    return render(request, "users/register.html", context)
+
 
 
 def user_login(request):
-    form = AuthenticationForm(data=request.POST)
+    
+    form=AuthenticationForm(data=request.POST)
     if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        return redirect("home")
-    context = {"form": form}
-    return render(request, "users/login.html", context)
+        user=form.get_user()
+        login(request,user)
+        messages.success(request,'login successfully')
+        return redirect('home')
+    context={
+        'form':form
+    }
+    return render(request,'users/login.html',context)
 
-
-def user_logout(request):   
+def user_logout(request):
     logout(request)
-    return redirect("home")
+    messages.warning(request,'logout successfully')
+    return redirect('home')
+   
